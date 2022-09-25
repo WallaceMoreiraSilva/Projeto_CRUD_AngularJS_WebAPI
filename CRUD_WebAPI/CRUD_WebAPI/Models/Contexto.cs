59c7b572d850e7840c6481c09af4b2233bbad0d0
Wallace M <wallaceinfofuturo@gmail.com>
2022-09-25 17:44:28 -0300

    passando back end para visual studio 2019

---
+++ CRUD_WebAPI/CRUD_WebAPI/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_WebAPI.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
